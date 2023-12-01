6763b8701905000cc61280615f17ec385e0f8405
HoangLong <84828423+ninhhoanglong@users.noreply.github.com>
2023-12-02 01:31:30 +0700

    fix localstorage is undefined

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,25 +4,20 @@ import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
-const getFromLocalStorage = () => {
-  if (typeof window !== undefined) {
-    const value = localStorage.getItem("theme");
-    return value || "light";
-  }
-};
-
 export const ThemeContextProvider = ({ children }) => {
-  const [theme, setTheme] = useState(() => {
-    return getFromLocalStorage();
-  });
-
-  const toggle = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    setTheme(storedTheme || "light");
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("theme", theme);
   }, [theme]);
+
+  const toggle = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
   return (
     <ThemeContext.Provider value={{ theme, toggle }}>
