6da8ad1cabc889bd61115bde5e4506b93a536d9e
HoangLong <84828423+ninhhoanglong@users.noreply.github.com>
2023-12-05 15:17:33 +0700

    update title document

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ import AuthProvider from "@/providers/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Blog App",
+  title: "Longnh Blog",
   description: "The best blog app!",
 };
 
